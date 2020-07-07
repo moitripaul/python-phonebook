e63b034e6efe5cf327413a6dbdbf05f6557ba8f8
moitripaul <moitripaul682@gmail.com>
2020-07-07 20:20:47 +0530

    created the basic window

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+def main():
+    root = Tk()
+    root.title("Phonebook App")
+    root.geometry("650x550+350+200")
+    root.resizable(False, False)
+    root.mainloop()
+
+if __name__ == '__main__' :
+    main()
