49833be9e43b4ff7fa13d7046fbeeac1688ba2c6
moitripaul <moitripaul682@gmail.com>
2020-07-10 21:31:41 +0530

    added sqlite database

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from tkinter import *
+import sqlite3
+
+
+conn = sqlite3.connect('phonebook.db')
+c = conn.cursor()
 
 class PhonebookApp(object):
     def __init__(self, master):
@@ -11,12 +16,23 @@ class PhonebookApp(object):
         #heading
         self.heading = Label(self.top, text='Phonebook App', font='Helvetica 20 bold', bg='white')
         self.heading.place(x=230, y= 25)
-        # buttons for add, view, update, delete
+        # buttons for create, read, update, delete
         self.createBtn = Button(self.bottom, text="add contacts", )
         self.createBtn.place(x=210, y=70)
-        self.readBtn = Button(self.bottom, text="read contacts", )
+        self.readBtn = Button(self.bottom, text="read contacts", command=self.readEntry )
         self.readBtn.place(x=210, y=130)
-        
+
+    def readEntry(self):
+        person = PersonInfo()
+
+
+class PersonInfo(Toplevel):
+    def __init__(self):
+        Toplevel.__init__(self)
+        self.geometry("650x650+600+200")
+        self.title("Contacts")
+        self.resizable(False,False)     
+
 
 def main():
     root = Tk() # root is GUI window
